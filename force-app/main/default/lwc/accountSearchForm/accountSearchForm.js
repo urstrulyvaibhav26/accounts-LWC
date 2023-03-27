0e26ca24c51e39f8d0dde8807454b3bec0de9e41
Vaibhav Kumar <urstrulyvaibhav026@gmail.com>
2023-03-28 00:00:37 +0530

    update

--- force-app/main/default/lwc/accountSearchForm/accountSearchForm.js
+++ force-app/main/default/lwc/accountSearchForm/accountSearchForm.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class AccountSearchForm extends LightningElement {}
+export default class AccountSearchForm extends LightningElement {
+    searchText = '';
+    accountNameHandler(event){
+        this.searchText = event.target.value;
+    }
+
+    searchHandler(){
+        console.log(this.searchText);
+    }
+}
